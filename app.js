79d1ddaa443899208318ff50b8a801ec62256537
Varalaxmipula <113462439+Varalaxmipula@users.noreply.github.com>
2023-01-08 23:38:40 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+
 const csrf = require("tiny-csrf");
 const cookieParser = require("cookie-parser");
 const { admin, Election, questions } = require("./models");
